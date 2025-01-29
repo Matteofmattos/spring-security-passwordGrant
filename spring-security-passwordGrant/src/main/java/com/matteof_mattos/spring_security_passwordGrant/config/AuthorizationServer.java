f334db8ca41b7e6c61490475e34437d900920f51
Matteo <Matheus.oli2016@outlook.com>
2025-01-29 10:15:25 -0300

    Registered client & tokenCustomizer adicionados

--- spring-security-passwordGrant/src/main/java/com/matteof_mattos/spring_security_passwordGrant/config/AuthorizationServer.java
+++ spring-security-passwordGrant/src/main/java/com/matteof_mattos/spring_security_passwordGrant/config/AuthorizationServer.java
@@ -2,6 +2,7 @@ package com.matteof_mattos.spring_security_passwordGrant.config;
 
 import com.matteof_mattos.spring_security_passwordGrant.config.supports.CustomPasswordAuthenticationConverter;
 import com.matteof_mattos.spring_security_passwordGrant.config.supports.CustomPasswordAuthenticationProvider;
+import com.matteof_mattos.spring_security_passwordGrant.config.supports.CustomUserAuthorities;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.JWKSource;
@@ -13,16 +14,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.OAuth2Token;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.authorization.InMemoryOAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.authentication.OAuth2ClientAuthenticationToken;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.oauth2.server.authorization.token.*;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -30,6 +40,8 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.time.Duration;
+import java.util.List;
 import java.util.UUID;
 
 @Configuration
@@ -42,7 +54,7 @@ public class AuthorizationServer {
     private String clientSecret;
 
     @Value("${security.jwt.duration}")
-    private String jwtDurationSeconds;
+    private Integer jwtDurationSeconds;
 
     @Autowired
     private UserDetailsService userDetailsService;
@@ -65,31 +77,24 @@ public class AuthorizationServer {
         return httpSecurity.build();
     }
 
-    @Bean
-    public OAuth2AuthorizationService authorizationService() {
-
-//        1- Recebe e valida pedidos de autorização de aplicativos cliente,
-//        processa e gerencia as decisões de autorização.
-
-//       2 - Armazena informações sobre autorizações concedidas ou negadas.
-//                Recupera essas informações para futuras referências.
-
-        return new InMemoryOAuth2AuthorizationService();
-    }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public RegisteredClientRepository registeredClientRepository(){
+
+        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId(clientId)
+                .clientSecret(passwordEncoder().encode(clientSecret))
+                .scope("read")
+                .scope("write")
+                .authorizationGrantType(new AuthorizationGrantType("password"))
+                .tokenSettings(tokenSettings())
+                .clientSettings(clientSettings())
+                .build();
+
+        return new InMemoryRegisteredClientRepository(registeredClient);
+
     }
 
-    @Bean
-    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
-        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
-    }
-
-
-
-    // -----------// jwt token generation //------------
 
     @Bean
     public OAuth2TokenGenerator<? extends OAuth2Token> tokenGenerator() {
@@ -101,8 +106,49 @@ public class AuthorizationServer {
     }
 
 
-    private OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer() {
-        return null;
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer() {
+
+        return context -> {
+            OAuth2ClientAuthenticationToken principal = context.getPrincipal();
+            CustomUserAuthorities details = (CustomUserAuthorities) principal.getDetails();
+            List<String> authorities = details.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+
+            if (context.getTokenType().getValue().equals("access_token")){
+                context.getClaims()
+                        .claim("authorities", authorities)
+                        .claim("username", details.getUsername());
+            };
+        };
+    }
+
+
+    @Bean
+    public TokenSettings tokenSettings() {
+        // @formatter:off
+        return TokenSettings.builder()
+                .accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
+                .accessTokenTimeToLive(Duration.ofSeconds(jwtDurationSeconds))
+                .build();
+        // @formatter:on
+    }
+
+    @Bean
+    public ClientSettings clientSettings() {
+        return ClientSettings.builder().build();
+    }
+
+
+    @Bean
+    public OAuth2AuthorizationService authorizationService() {
+
+//        1- Recebe e valida pedidos de autorização de aplicativos cliente,
+//        processa e gerencia as decisões de autorização.
+
+//       2 - Armazena informações sobre autorizações concedidas ou negadas.
+//                Recupera essas informações para futuras referências.
+
+        return new InMemoryOAuth2AuthorizationService();
     }
 
 
@@ -130,5 +176,16 @@ public class AuthorizationServer {
             throw new IllegalStateException(ex);
         }
         return keyPair;
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
     }
 }
